9c6b820eb102dafa829f45f6b9239084c1fd854a
Sonia-88 <sonia.bevilacqua@studenti.unicam.it>
2025-06-20 09:00:06 +0200

    Creazione Interfaccia.

---
+++ ProgettoFilieraAgricola/src/main/java/com/ids/ProgettoIDS/Repositories/AnagraficaRepository.java
@@ -0,0 +1,7 @@
+package com.ids.ProgettoIDS.Repositories;
+
+import com.ids.ProgettoIDS.Model.Anagrafica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnagraficaRepository extends JpaRepository<Anagrafica, Integer> {
+}
